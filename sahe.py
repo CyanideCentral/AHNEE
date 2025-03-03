5abeb797693b5573132e5ca5ebbe0e19ed35f378
LYR <liyiranymula@gmail.com>
2025-03-03 17:39:12 +0800

    comments & gitignore

--- sahe.py
+++ sahe.py
@@ -4,37 +4,38 @@ import time
 import resource
 import config
 from sklearn.preprocessing import normalize
-import pickle as pkl
 import faiss
 from sparse_dot_mkl import dot_product_mkl
 from pts import pts_tlog
 
-def extend_hypergraph(ahg, knn = 10, weighted = True, index_type = "IVF512,PQ10", return_index = False):
+def extend_hypergraph(ahg, knn = 10, index_type = "IVF512,PQ10"):
     attributes = ahg.attributes.toarray()
     n, d = attributes.shape
     faiss.normalize_L2(attributes)
-    if n > 100000:
+    if n > 100000: # Use IVF for large datasets
         index = faiss.index_factory(d, index_type, faiss.METRIC_INNER_PRODUCT)
         index.train(attributes)
-    else:
+    else: # exact search for small datasets
         index = faiss.IndexFlatIP(d)
     index.add(attributes)
     D, I = index.search(attributes, knn+1)
-    H = sp.csr_matrix((D.flatten(), I.flatten(), np.arange(0, n*(knn+1)+1, knn+1)), shape=(n, n))
-    self_weight = 1.0
-    H.setdiag(self_weight)
+    # Construct the incidence matrix H_K
+    H_K = sp.csr_matrix((D.flatten(), I.flatten(), np.arange(0, n*(knn+1)+1, knn+1)), shape=(n, n))
+    H_K.setdiag(1.0)
 
-    aug_hg = config.beta* H
-    athg = sp.vstack([ahg.hypergraph, aug_hg], format='csr')
-    H_vol = (ahg.hypergraph.sum(1).A1).sum()
-    A_vol = (aug_hg.sum(1).A1).sum()
+    # Extended hypergraph; Balance via hyperedge weighting
+    H_K = config.beta * H_K
+    extended_H = sp.vstack([ahg.hypergraph, H_K], format='csr')
+    H_vol = (ahg.hypergraph.sum(1).A1).sum() # Volume of H_0
+    A_vol = (H_K.sum(1).A1).sum() # Volume of H_K
     attr_weight = H_vol / (H_vol + A_vol)
-    he_weights = np.concatenate([np.ones(ahg.m), np.full(aug_hg.shape[0], attr_weight)])
-    return athg, he_weights
+    # Hyperedge weights
+    he_weights = np.concatenate([np.ones(ahg.m), np.full(H_K.shape[0], attr_weight)])
+    return extended_H, he_weights
 
 def sahe(ahg):
     start_time = time.perf_counter()
-    H, hyperedge_weights= extend_hypergraph(ahg, knn=config.knn, weighted=True)
+    H, hyperedge_weights= extend_hypergraph(ahg, knn=config.knn)
     config.rng = np.random.default_rng(0)
     vol = float(H.sum())
     m, n = H.shape
@@ -46,7 +47,8 @@ def sahe(ahg):
     deg_edge[deg_edge==0] = 1
     D_e = sp.diags(deg_edge**(-0.5))
     D_e = sp.diags(hyperedge_weights**0.5) @ D_e
-    
+
+    # Common computations for nodes and hyperedges
     norm_H = D_e @ H @ D_v # \tilde{H}
     # Truncated SVD
     def linear_operator(x):
@@ -59,22 +61,23 @@ def sahe(ahg):
     sigma_T = np.full_like(sigma_sq, config.alpha)
     for i in range(1, config.window+1):
         sigma_T += config.alpha * (1- config.alpha)**i * sigma_sq**i
-    
+
+    # Approximating HMS-N node similarity
     F_v = np.sqrt(vol) * sp.diags(np.sqrt(sigma_T)).dot(VT@D_v).T
     Y_v, Th_v = pts_tlog(F_v)
     YTh_v = Y_v @ Th_v
-    def laplacian_operator(x):
-        return Y_v @ (YTh_v.T @ x)
+    def laplacian_operator(v):
+        return Y_v @ (YTh_v.T @ v) # L(v) = Y (\Theta Y (v))
     Gamma_v_LO = sp.linalg.LinearOperator((n, n), matvec=laplacian_operator)
     _, Z_v = sp.linalg.eigsh(Gamma_v_LO, min(config.embedding_dim, n//2), tol=config.svd_tol,which='LM')
     Z_v = normalize(Z_v, norm='l2', axis=1)
 
-    D_rt_invU = D_e.dot(U)[:m-n]
+    # Approximating HMS-E hyperedge similarity
     F_e = np.sqrt(vol)*sp.diags(np.sqrt(sigma_T)).dot(D_e.dot(U)[:m-n].T).T
     Y_e, Th_e = pts_tlog(F_e)
     YTh_e = Y_e @ Th_e
-    def laplacian_operator(x):
-        return Y_e @ (YTh_e.T @ x)
+    def laplacian_operator(v):
+        return Y_e @ (YTh_e.T @ v)  # L(v) = Y (\Theta Y (v))
     Gamma_e_LO = sp.linalg.LinearOperator((m-n, m-n), matvec=laplacian_operator)
     _, Z_e = sp.linalg.eigsh(Gamma_e_LO, min(config.embedding_dim, (m-n)//2), tol=config.svd_tol,which='LM')
     Z_e = normalize(Z_e, norm='l2', axis=1)
